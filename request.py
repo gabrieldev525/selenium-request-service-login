0c735af15ebd7b9986da6640700c2429b092d6af
gabrieldev525 <gvictor525.gv@gmail.com>
2019-08-14 17:16:41 -0300

    tests using now python requests and beautiful soup

--- request.py
+++ request.py
@@ -1,6 +1,19 @@
+from bs4 import BeautifulSoup
 import requests
+import ipdb
 
-r = requests.post('https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&amp;lwv=110', data={'email':'gvictor525.gv@gmail.com', 'password':'123'})
-return_code = r.status_code
 
+facebook_headers = {
+  'content-type': 'application/json',
+  'Host': 'www.facebook.com',
+  'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'
+}
+
+response = requests.post('https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&amp;lwv=110', data={'email':'gvictor525.gv@gmail.com', 'pass':'123'}, headers=facebook_headers)
+return_code = response.status_code
 print(return_code)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.prettify())
+
+ipdb.set_trace()
